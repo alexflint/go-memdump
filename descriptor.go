0b667ec9d76ca41ff7220e7115ac9deeb2cffe6a
Alex Flint <alex.flint@gmail.com>
2016-04-20 11:02:37 -0700

    add descriptor with tests

---
+++ descriptor.go
@@ -0,0 +1,124 @@
+package memdump
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// pointer - type
+// slice - type
+// array - type, length
+// string
+// int - size
+// float - size
+
+// A Descriptor describes a type such that if two types have the
+// same descriptor then their memory layout is identical.
+type descriptor []typ
+
+// a class contains information about a type
+type typ struct {
+	Kind   reflect.Kind // Kind is the kind of this type
+	Size   uintptr      // Size is the size in bits, as per reflect.Value.Size
+	Elem   int          // Elem is index of the underlying type for pointers, slices, and arrays
+	Fields []field      // Fields contains the fields for structs
+}
+
+type field struct {
+	Name   string  // Name is the name of this field, or its memdump tag if present
+	Offset uintptr // Offset is the position of this field relative to the beginning of the struct
+	Type   int     // ID is the index of the type of this field in the descripto
+}
+
+// descriptorsEqual compares two descriptors
+func descriptorsEqual(a, b descriptor) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i].Kind != b[i].Kind {
+			return false
+		}
+		if a[i].Size != b[i].Size {
+			return false
+		}
+		switch a[i].Kind {
+		case reflect.Array, reflect.Slice, reflect.Ptr:
+			if a[i].Elem != b[i].Elem {
+				return false
+			}
+		case reflect.Struct:
+			if len(a[i].Fields) != len(b[i].Fields) {
+				return false
+			}
+			for j := range a[i].Fields {
+				if a[i].Fields[j].Name != b[i].Fields[j].Name {
+					return false
+				}
+				if a[i].Fields[j].Offset != b[i].Fields[j].Offset {
+					return false
+				}
+				if a[i].Fields[j].Type != b[i].Fields[j].Type {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+// describe computes the descriptor for a type
+func describe(t reflect.Type) descriptor {
+	var nextID int
+	var desc descriptor
+	var queue []reflect.Type
+	seen := make(map[reflect.Type]int)
+
+	push := func(t reflect.Type) int {
+		if id, found := seen[t]; found {
+			return id
+		}
+		id := nextID
+		seen[t] = id
+		nextID++
+		queue = append(queue, t)
+		return id
+	}
+
+	push(t)
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		t := typ{
+			Size: cur.Size(),
+			Kind: cur.Kind(),
+		}
+
+		switch cur.Kind() {
+		case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map:
+			panic(fmt.Sprintf("cannot compute descriptor for %v", cur.Kind()))
+		case reflect.Array, reflect.Slice, reflect.Ptr:
+			t.Elem = push(cur.Elem())
+		case reflect.Struct:
+			for i := 0; i < cur.NumField(); i++ {
+				f := cur.Field(i)
+				if f.Type.Size() == 0 {
+					continue
+				}
+
+				name := f.Name
+				if tag := f.Tag.Get("memdump"); tag != "" {
+					name = tag
+				}
+				t.Fields = append(t.Fields, field{
+					Name:   name,
+					Offset: f.Offset,
+					Type:   push(f.Type),
+				})
+			}
+		}
+
+		desc = append(desc, t)
+	}
+	return desc
+}
---
+++ descriptor_test.go
@@ -0,0 +1,135 @@
+package memdump
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/kr/pretty"
+	"github.com/stretchr/testify/assert"
+)
+
+func assertCompareDescriptors(t *testing.T, a interface{}, b interface{}, expected bool) {
+	da := describe(reflect.TypeOf(a))
+	db := describe(reflect.TypeOf(b))
+	if !assert.Equal(t, expected, descriptorsEqual(da, db)) {
+		pretty.Println(da)
+		pretty.Println(db)
+	}
+}
+
+func TestDescribeScalar(t *testing.T) {
+	assertCompareDescriptors(t, "abc", "d", true)
+	assertCompareDescriptors(t, 123, 456, true)
+	assertCompareDescriptors(t, 1.2, 3.4, true)
+	assertCompareDescriptors(t, uint64(12), uint64(34), true)
+
+	assertCompareDescriptors(t, int16(12), int64(34), false)
+	assertCompareDescriptors(t, uint64(12), int64(34), false)
+	assertCompareDescriptors(t, uint64(12), "34", false)
+	assertCompareDescriptors(t, uint64(12), 4.5, false)
+}
+
+func TestDescribePointer(t *testing.T) {
+	var x int
+	var y uint64
+	var z string
+
+	assertCompareDescriptors(t, &x, &x, true)
+	assertCompareDescriptors(t, &y, &y, true)
+	assertCompareDescriptors(t, &z, &z, true)
+
+	assertCompareDescriptors(t, &x, &y, false)
+	assertCompareDescriptors(t, &x, &z, false)
+	assertCompareDescriptors(t, &x, x, false)
+
+	xptr := &x
+	xptrptr := &xptr
+
+	assertCompareDescriptors(t, xptrptr, xptrptr, true)
+	assertCompareDescriptors(t, xptr, xptrptr, false)
+	assertCompareDescriptors(t, x, xptrptr, false)
+}
+
+func TestDescribeSlice(t *testing.T) {
+	var x []int
+	var y []uint64
+	var z [][]int16
+
+	assertCompareDescriptors(t, x, x, true)
+	assertCompareDescriptors(t, y, y, true)
+	assertCompareDescriptors(t, z, z, true)
+	assertCompareDescriptors(t, &x, &x, true)
+	assertCompareDescriptors(t, &y, &y, true)
+	assertCompareDescriptors(t, &z, &z, true)
+
+	assertCompareDescriptors(t, x, y, false)
+	assertCompareDescriptors(t, &x, &y, false)
+	assertCompareDescriptors(t, x, z, false)
+	assertCompareDescriptors(t, &x, x, false)
+}
+
+func TestDescribeArray(t *testing.T) {
+	var x [4]int
+	var y [10]int
+	var z [2][3]int16
+
+	assertCompareDescriptors(t, x, x, true)
+	assertCompareDescriptors(t, y, y, true)
+	assertCompareDescriptors(t, z, z, true)
+	assertCompareDescriptors(t, &x, &x, true)
+	assertCompareDescriptors(t, &y, &y, true)
+	assertCompareDescriptors(t, &z, &z, true)
+
+	assertCompareDescriptors(t, x, y, false)
+	assertCompareDescriptors(t, &x, &y, false)
+	assertCompareDescriptors(t, x, z, false)
+	assertCompareDescriptors(t, &x, x, false)
+}
+
+func TestDescribeStruct(t *testing.T) {
+	type u struct {
+		A string
+		B int
+	}
+	type v struct {
+		A string
+		B int32
+	}
+	type w struct {
+		u
+		A string
+		B int
+		c v
+	}
+
+	assertCompareDescriptors(t, u{}, u{}, true)
+	assertCompareDescriptors(t, v{}, v{}, true)
+	assertCompareDescriptors(t, w{}, w{}, true)
+	assertCompareDescriptors(t, &u{}, &u{}, true)
+	assertCompareDescriptors(t, &v{}, &v{}, true)
+	assertCompareDescriptors(t, &w{}, &w{}, true)
+
+	assertCompareDescriptors(t, u{}, v{}, false)
+	assertCompareDescriptors(t, u{}, w{}, false)
+	assertCompareDescriptors(t, v{}, w{}, false)
+	assertCompareDescriptors(t, u{}, &u{}, false)
+}
+
+func TestDescribeStructWithTags(t *testing.T) {
+	type u struct {
+		A string
+		B int
+	}
+	type v struct {
+		A   string
+		xyz int
+	}
+	type w struct {
+		A string
+		B int `memdump:"xyz"`
+	}
+
+	assertCompareDescriptors(t, u{}, v{}, false)
+	assertCompareDescriptors(t, u{}, w{}, false)
+	assertCompareDescriptors(t, v{}, w{}, true)
+}
